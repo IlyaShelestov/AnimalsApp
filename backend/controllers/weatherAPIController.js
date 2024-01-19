f366a33960f49b0308c94ce9462c3bd8d3b7002f
IlyaShelestov <shelestov_ilya2905@mail.ru>
2024-01-20 02:14:37 +0600

    Finished weather retrieving

--- backend/controllers/weatherAPIController.js
+++ backend/controllers/weatherAPIController.js
@@ -1,10 +1,9 @@
-const https = require('node:https');
+const https = require("node:https");
 const config = require("../config.js");
-
 
 function getWeatherData(city) {
   return new Promise((resolve, reject) => {
-    const url =`${config.openWeatherMap.apiUrl}?q=${city}&appid=${config.openWeatherMap.apiKey}&units=metric`;
+    const url = `${config.openWeatherMap.apiUrl}?q=${city}&appid=${config.openWeatherMap.apiKey}&units=metric`;
     const weatherData = {};
 
     https.get(url, (res) => {
@@ -12,10 +11,11 @@ function getWeatherData(city) {
         let APIdata = JSON.parse(data);
         weatherData.lon = APIdata.coord.lon;
         weatherData.lat = APIdata.coord.lat;
+        weatherData.main = APIdata.weather[0].main;
         weatherData.description = APIdata.weather[0].description;
         weatherData.icon = APIdata.weather[0].icon;
         weatherData.feels_like = APIdata.main.feels_like;
-        weatherData.temp = APIdata.main.temp;
+        weatherData.temperature = APIdata.main.temp;
         weatherData.pressure = APIdata.main.pressure;
         weatherData.humidity = APIdata.main.humidity;
         weatherData.wind = APIdata.wind.speed;
@@ -27,4 +27,4 @@ function getWeatherData(city) {
   });
 }
 
-module.exports = {getWeatherData};
+module.exports = { getWeatherData };
--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const weatherAPIController = require("../controllers/weatherAPIController.js")
+const weatherAPIController = require("../controllers/weatherAPIController.js");
 const router = express.Router();
 
-router.get("/weather", (req, res) => {
-  res.render("weather", { city: "Astana" });
+router.get("/weather", async (req, res) => {
+  try {
+    let weatherData = await weatherAPIController.getWeatherData("Astana");
+    res.render("weather", { weatherData, city: "Astana" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching weather data");
+  }
 });
 
 router.post("/weather", async (req, res) => {
   try {
     let city = req.body.city;
     let weatherData = await weatherAPIController.getWeatherData(city);
-    console.log(weatherData) + "qwewewer";
-    res.render("weather", { city: weatherData.humidity });
+    res.render("weather", { weatherData, city: city });
   } catch (error) {
     console.error(error);
     res.status(500).send("Error fetching weather data");
